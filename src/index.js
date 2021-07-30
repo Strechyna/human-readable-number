fa811aceb1280a8f4d9fad6880c1776f7192b3b9
strechyna <brouillasse@gmail.com>
2021-07-30 22:32:05 +0300

    add Template literals

--- src/index.js
+++ src/index.js
@@ -2,40 +2,40 @@ const FROM_ZERO_TO_NINETEEN = ['zero', 'one', 'two', 'three', 'four', 'five', 's
 const FROM_TWENTY_TO_NINETY = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
 
-module.exports = function toReadable (number) {
-  if (number == null || isNaN(number)) {
-      return '';
-  }
+module.exports = function toReadable(number) {
+    if (number == null || isNaN(number)) {
+        return '';
+    }
 
-  var definitelyNumber = Number(number);
+    const definitelyNumber = Number(number);
 
-  var n2 = definitelyNumber % 100;
-  var n3 = (definitelyNumber - n2) / 100;
+    const n2 = definitelyNumber % 100;
+    const n3 = (definitelyNumber - n2) / 100;
 
-  var result = '';
+    let result = '';
 
-  if (n3 != 0) {
-      result = FROM_ZERO_TO_NINETEEN[n3] + ' hundred';
-      if (n2 != 0) {
-          result += ' ' + getValueFromZeroToNinetyNine(n2);
-      }
-  } else {      
-      result += getValueFromZeroToNinetyNine(n2);
-  }
+    if (n3 != 0) {
+        result = `${FROM_ZERO_TO_NINETEEN[n3]} hundred`;
+        if (n2 != 0) {
+            result += ` ${getValueFromZeroToNinetyNine(n2)}`;
+        }
+    } else {
+        result += getValueFromZeroToNinetyNine(n2);
+    }
 
-  return result;
+    return result;
 }
 
 function getValueFromZeroToNinetyNine(number) {
     if (number < 20) {
         return FROM_ZERO_TO_NINETEEN[number];
     } else {
-        var n1 = number % 10;
-        var n2 = (number - n1) / 10;
+        const n1 = number % 10;
+        const n2 = (number - n1) / 10;
 
-        var result = FROM_TWENTY_TO_NINETY[n2 - 2];
+        let result = FROM_TWENTY_TO_NINETY[n2 - 2];
         if (n1 != 0) {
-            result += ' ' + FROM_ZERO_TO_NINETEEN[n1];
+            result += ` ${FROM_ZERO_TO_NINETEEN[n1]}`;
         }
 
         return result;
